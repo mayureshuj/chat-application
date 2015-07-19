74185931d535f34bc6891ec3c7c14ad24322fb09
Mayuresh <mayureshuj@gmail.com>
2015-07-19 23:20:34 +0530

    Basic chat application

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
-var express = require("express");
-var app = express();
-var port = 3700;
- 
-app.get("/", function(req, res){
-    res.send("It works!");
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
 });
- 
-app.listen(port);
-console.log("server runing on : http://localhost:" + port);
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
